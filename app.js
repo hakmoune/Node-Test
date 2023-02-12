4c6a22a206f9c4174a629f4dd2976907436d26e0
mehdi <mehdi.hakmoune@gmail.com>
2023-02-12 23:30:32 +0000

    Ajouter une serie de meddleware comme exmple

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const express = require("express"); // Importer Express
 const app = express(); // Creer un application Express
 
 //un middleware
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log("Requite Recue!!");
+  next();
+});
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
   res.json({ message: "votre requite est bien" });
+  next();
+});
+
+app.use((req, res) => {
+  console.log("La reponse a été envoyer avec success");
 });
 
 module.exports = app; // export app pour puisse l'acceeder dans notre application

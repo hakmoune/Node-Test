5207b738397b91ecfa42b6a89d14ef809941f7ca
mehdi <mehdi.hakmoune@gmail.com>
2023-02-13 18:41:59 +0000

    Create our first API GET

--- app.js
+++ app.js
@@ -2,7 +2,50 @@ const express = require("express"); // Importer Express
 
 const app = express(); // Pour créer une application Express
 
-//un middleware
+//Headrs allow our API to work on deferent servers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // * = d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  ); // d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  ); // d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+  next();
+});
+
+//Middleware
+//la route
+app.use("/api/stuff", (req, res, next) => {
+  //Les données
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios"
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios"
+    }
+  ];
+  res.status(200).json(stuff); // Nous envoyons ensuite ces articles sous la forme de données JSON, avec un code 200 pour une demande réussie.
+});
+
+module.exports = app; // export app pour puisse l'acceeder dans notre application
+
+/*un middleware
 app.use((req, res, next) => {
   console.log("Requite Recue!!");
   next();
@@ -19,6 +62,4 @@ app.use((req, res, next) => {
 
 app.use((req, res) => {
   console.log("La reponse a été envoyer avec success");
-});
-
-module.exports = app; // export app pour puisse l'acceeder dans notre application
+});*/

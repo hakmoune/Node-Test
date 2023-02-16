e5379e745c53836d321c2222fda1d6905f7330ac
mehdi <mehdi.hakmoune@gmail.com>
2023-02-16 17:38:49 +0000

    Optimisez la structure du back-end: 1.Les routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express"); // Importer Express
-const mongoose = require("mongoose");
-
-const Thing = require("./models/Thing");
+const mongoose = require("mongoose"); // Import mongoose
 
 const app = express(); // Pour créer une application Express
 
+const stuffRoutes = require("./routes/stuff"); //Importer notre stuff's routes
+
+//Etablir la cnx avec notre DB
 mongoose
   .connect(
     "mongodb+srv://mehdi_1990:mehdi_1990@cluster0.85jqavd.mongodb.net/?retryWrites=true&w=majority",
@@ -12,8 +13,6 @@ mongoose
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-app.use(express.json()); //Ce middleware intercept toutes les requêtes qui ont un content Type JSON
 
 //Headrs allow our API to work on deferent servers (Surpase CROS error)
 app.use((req, res, next) => {
@@ -29,50 +28,8 @@ app.use((req, res, next) => {
   next();
 });
 
-//Middleware ADD
-app.post("/api/stuff", (req, res, next) => {
-  //delete req.body.userId; // Pour supprimer un attr depuis notre objet
-  const thing = new Thing({
-    ...req.body // L'opérateur spread ... est utilisé pour faire une copie de tous les éléments de req.body
-  });
+app.use(express.json()); //Ce middleware intercept toutes les requêtes qui ont un content Type JSON
 
-  thing
-    .save() // pour enregistrer votre Thing dans la base de données.
-    .then(() =>
-      res.status(201).json({
-        resData: thing
-      })
-    )
-    .catch(error => res.status(400).json({ error }));
-});
-
-//Middleware Git ALL
-//nous utilisons la méthode get() pour répondre uniquement aux demandes GET à cet endpoint ;
-app.get("/api/stuff", (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//Middleware Git One
-app.get("/api/stuff/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(400).json({ error })); // si aucun Thing n'est trouvé ou si une erreur se produit, nous envoyons une erreur 404 au front-end, avec l'erreur générée
-});
-
-//Middleware PUT/Update
-app.put("/api/stuff/:id", (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ resData: req.body }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//Middleware Delete
-app.delete("/api/stuff/:id", (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Bien Supprimé !" }))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use("/api/stuff", stuffRoutes); // Enregistrer nos routeurs dans notre application Express
 
 module.exports = app; // export app pour puisse l'acceeder dans notre application
